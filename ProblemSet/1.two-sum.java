5f40391c5c98bb95874b5f897b959db1ee64f93b
xtl4350 <ix22824953@sina.com>
2020-06-16 18:00:08 +0800

    1.two-sum

---
+++ ProblemSet/1.two-sum.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(target - nums[i])) {
+                ans[0] = map.get(target - nums[i]);
+                ans[1] = i;
+                break;
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
