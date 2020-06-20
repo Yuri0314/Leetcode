a918dbae2f7acaee10023ffa3a43802819829467
xtl4350 <ix22824953@sina.com>
2020-06-20 18:21:25 +0800

    206.reverse-linked-list 迭代法

---
+++ ProblemSet/206.reverse-linked-list 迭代法.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return head;
+        ListNode p1 = null, p2 = head, p3;
+        while (p2 != null) {
+            p3 = p2.next;
+            p2.next = p1;
+            p1 = p2;
+            p2 = p3;
+        }
+        return p1;
+    }
+}
+// @lc code=end
+
