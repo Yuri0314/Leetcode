8afc9da0398a306879b5818e50b7f0be53e43da6
xtl4350 <ix22824953@sina.com>
2020-06-21 16:32:35 +0800

    24.swap-nodes-in-pairs 迭代法

---
+++ ProblemSet/24.swap-nodes-in-pairs 迭代法.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=24 lang=java
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode guard = new ListNode(0);
+        guard.next = head;
+        ListNode pre = guard;
+        while (head != null && head.next != null) {
+            pre.next = head.next;
+            head.next = head.next.next;
+            pre.next.next = head;
+            pre = head;
+            head = head.next;
+        }
+        return guard.next;
+    }
+}
+// @lc code=end
+
