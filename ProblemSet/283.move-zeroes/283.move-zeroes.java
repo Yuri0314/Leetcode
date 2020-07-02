7eb2cc88cd56fb5a228f5055e1940f9aaf5763e9
xtl4350 <ix22824953@sina.com>
2020-07-02 10:03:26 +0800

    fix

--- ProblemSet/283.move-zeroes/283.move-zeroes.java
+++ ProblemSet/283.move-zeroes/283.move-zeroes.java
@@ -7,12 +7,12 @@
 // @lc code=start
 class Solution {
     public void moveZeroes(int[] nums) {
-        int cur, nonZeroTail;
         if (nums.length <= 0)
             return;
-        for (cur = nonZeroTail = 0; cur < nums.length; ++cur) {
-            if (nums[cur] == 0) continue;
-            else nums[nonZeroTail++] = nums[cur];
+        int nonZeroTail = 0;
+        for (int cur = 0; cur < nums.length; ++cur) {
+            if (nums[cur] != 0)
+                nums[nonZeroTail++] = nums[cur];
         }
         while (nonZeroTail < nums.length)
             nums[nonZeroTail++] = 0;
