88f02c14e2dee32ea73275bb908616dc3628fa18
xtl4350 <ix22824953@sina.com>
2020-07-04 16:19:07 +0800

    242.valid-anagram 哈希表法

---
+++ ProblemSet/242.valid-anagram/242.valid-anagram 哈希表法.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=242 lang=java
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int num : counter)
+            if (num != 0) return false;
+        return true;
+    }
+}
+// @lc code=end
+
