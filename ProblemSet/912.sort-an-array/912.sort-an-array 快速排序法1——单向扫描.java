3641a5af52d49e2cfff11873ac5a186f4be3db16
xtl4350 <ix22824953@sina.com>
2020-09-01 12:07:14 +0800

    912.sort-an-array 快速排序法

---
+++ ProblemSet/912.sort-an-array/912.sort-an-array 快速排序法1——单向扫描.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+/*
+ * @lc app=leetcode id=912 lang=java
+ *
+ * [912] Sort an Array
+ */
+
+// @lc code=start
+class Solution {
+    public int[] sortArray(int[] nums) {
+        return quickSort(nums, 0, nums.length - 1);
+    }
+
+    private int[] quickSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int pos = partition(nums, left, right);
+            quickSort(nums, left, pos - 1);
+            quickSort(nums, pos + 1, right);
+        }
+        return nums;
+    } 
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = new Random().nextInt(right - left + 1) + left;
+        swap(nums, left, pivot);
+        int i = left;
+        for (int j = i + 1; j <= right; ++j) {
+            if (nums[j] < nums[left]) 
+                swap(nums, ++i, j);
+        }
+        swap(nums, left, i);
+        return i;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+}
+// @lc code=end
+
