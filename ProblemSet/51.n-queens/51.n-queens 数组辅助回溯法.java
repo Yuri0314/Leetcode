5f41ad9cfa8e195cb2f5a691aaeb5d63da5100c0
xtl4350 <ix22824953@sina.com>
2020-09-03 22:43:19 +0800

    51.n-queens

---
+++ ProblemSet/51.n-queens/51.n-queens 数组辅助回溯法.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=51 lang=java
+ *
+ * [51] N-Queens
+ */
+
+// @lc code=start
+class Solution {
+    private List<List<String>> ans = new ArrayList<List<String>>();
+    private int[] queens;   // 存储皇后位置
+    private boolean[] cols;
+    private boolean[] upDiagonal;
+    private boolean[] downDiagonal;
+    private int n;
+    public List<List<String>> solveNQueens(int n) {
+        queens = new int[n];
+        cols = new boolean[n];
+        upDiagonal = new boolean[2 * n - 1];
+        downDiagonal = new boolean[2 * n];
+        this.n = n;
+        search(0);
+        return ans;
+    }
+
+    private void search(int row) {
+        if (row == n) {
+            List<String> solution = drawSolution();
+            ans.add(solution);
+            return;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (!cols[i] && !upDiagonal[row + i] && !downDiagonal[row - i + n]) {
+                queens[row] = i;
+                cols[i] = true;
+                upDiagonal[row + i] = true;
+                downDiagonal[row - i + n] = true;
+                search(row + 1);
+                // 回溯结束时无需恢复queens[row]位置，因为会覆盖
+                cols[i] = false;
+                upDiagonal[row + i] = false;
+                downDiagonal[row - i + n] = false;
+            }
+        }
+    }
+
+    private List<String> drawSolution() {
+        List<String> solution = new ArrayList<String>();
+        for (int i = 0; i < n; ++i) {
+            char[] row = new char[n];
+            Arrays.fill(row, '.');
+            row[queens[i]] = 'Q';
+            solution.add(new String(row));
+        }
+        return solution;
+    }
+}
+// @lc code=end
+
