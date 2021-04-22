7248b55e63fdb1bf476090aa1cf339b672206910
xtl4350 <ix22824953@sina.com>
2021-04-22 11:23:00 +0800

    ...

---
+++ ProblemSet/78. 子集/Solution.java
@@ -0,0 +1,22 @@
+// 时间复杂度O(n*2^n)，共2^n个状态，每个状态O(n)构造
+// 空间复杂度O(n)
+class Solution {
+    List<Integer> t = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(0, nums);
+        return ans;
+    }
+
+    public void dfs(int cur, int[] nums) {
+        if (cur == nums.length) {
+            ans.add(new ArrayList<Integer>(t));
+            return;
+        }
+        t.add(nums[cur]);
+        dfs(cur + 1, nums);
+        t.remove(t.size() - 1);
+        dfs(cur + 1, nums);
+    }
+}
