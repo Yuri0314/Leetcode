16d8d585fe1b899c00763ab8b5858e63eac90769
xtl4350 <ix22824953@sina.com>
2021-04-27 11:24:59 +0800

    ...

---
+++ ProblemSet/1299. 将每个元素替换为右侧最大元素/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        ans[n - 1] = -1;
+        for (int i = n - 2; i >= 0; --i) {
+            ans[i] = Math.max(ans[i + 1], arr[i + 1]);
+        }
+        return ans;
+    }
+}
