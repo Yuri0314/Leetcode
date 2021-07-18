8d64911f39a2416c070a24412ac293dc4fd56e2a
xtl4350 <ix22824953@sina.com>
2021-07-18 23:53:43 +0800

    fix

--- ProblemSet/912.sort-an-array/912.sort-an-array 归并排序法——递归实现.java
+++ ProblemSet/912.sort-an-array/912.sort-an-array 归并排序法——递归实现.java
@@ -23,7 +23,7 @@ class Solution {
 
     private void merge(int[] nums, int[] tmp, int lstart, int lend, int rstart, int rend) {
         int i = lstart, j = rstart, pos = lstart;
-        while (i <= lend && j <= rend) tmp[pos++] = nums[i] < nums[j] ? nums[i++] : nums[j++];
+        while (i <= lend && j <= rend) tmp[pos++] = nums[i] <= nums[j] ? nums[i++] : nums[j++];
         while (i <= lend) tmp[pos++] = nums[i++];
         while (j <= rend) tmp[pos++] = nums[j++];
         for (int k = lstart; k <= rend; ++k) nums[k] = tmp[k];
--- ProblemSet/912.sort-an-array/912.sort-an-array 归并排序法——非递归实现.java
+++ ProblemSet/912.sort-an-array/912.sort-an-array 归并排序法——非递归实现.java
@@ -34,7 +34,7 @@ class Solution {
 
     private void merge(int[] nums, int[] tmp, int lstart, int lend, int rstart, int rend) {
         int i = lstart, j = rstart, pos = lstart;
-        while (i <= lend && j <= rend) tmp[pos++] = nums[i] < nums[j] ? nums[i++] : nums[j++];
+        while (i <= lend && j <= rend) tmp[pos++] = nums[i] <= nums[j] ? nums[i++] : nums[j++];
         while (i <= lend) tmp[pos++] = nums[i++];
         while (j <= rend) tmp[pos++] = nums[j++];
     }
