3570e3492c6d69c427e6c0b675b7a78df035fbaa
陆洵 <huitianyu.hty@alibaba-inc.com>
2021-07-21 20:58:54 +0800

    49. 字母异位词分组

---
+++ ProblemSet/49. 字母异位词分组/Solution 排序.java
@@ -0,0 +1,16 @@
+// 时间复杂度：O(nklogk)，其中 n 是 strs 中的字符串的数量，k 是 strs 中的字符串的的最大长度。
+// 空间复杂度：O(nk)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
