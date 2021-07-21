cd1de6885741803e5537b5c48112e36dbf6b411c
xtl4350 <ix22824953@sina.com>
2021-07-21 22:14:49 +0800

    增加说明

--- ProblemSet/48. 旋转图像/Solution 旋转.java
+++ ProblemSet/48. 旋转图像/Solution 旋转.java
@@ -1,4 +1,13 @@
 // 时间复杂度O(N^2)
+/**
+ * temp = matrix[row][col]
+ * matrix[row][col] = matrix[n−col−1][row]
+ * matrix[n−col−1][row] = matrix[n−row−1][n−col−1]
+ * matrix[n−row−1][n−col−1] = matrix[col][n−row−1]
+ * matrix[col][n−row−1] = temp
+ * 当 n 为偶数时，我们需要枚举 n^2 / 4 = (n/2)×(n/2) 个位置
+ * 当 nn 为奇数时，由于中心的位置经过旋转后位置不变，我们需要枚举 (n^2-1) / 4 = ((n−1)/2)×((n+1)/2) 个位置
+*/
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
