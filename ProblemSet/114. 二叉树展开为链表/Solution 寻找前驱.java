0708798a1ae1df52d8a7ac405f3d8dcc276c48a2
xtl4350 <ix22824953@sina.com>
2021-07-26 16:51:05 +0800

    fix

--- ProblemSet/114. 二叉树展开为链表/Solution 寻找前驱.java
+++ ProblemSet/114. 二叉树展开为链表/Solution 寻找前驱.java
@@ -1,14 +1,16 @@
+/* 对于当前节点，如果其左子节点不为空，则在其左子树中找到最右边的节点，作为前驱节点，将当前节点的右子节点赋给前驱节点的右子节点，然后将当前节点的左子节点赋给当前节点的右子节点，并将当前节点的左子节点设为空。对当前节点处理结束后，继续处理链表中的下一个节点，直到所有节点都处理结束。
+ */
 class Solution {
     public void flatten(TreeNode root) {
         TreeNode curr = root;
         while (curr != null) {
             if (curr.left != null) {
                 TreeNode next = curr.left;
-                TreeNode predecessor = next;
-                while (predecessor.right != null) {
-                    predecessor = predecessor.right;
+                TreeNode tmp = next;
+                while tmp != null) {
+                    tmp = tmp.right;
                 }
-                predecessor.right = curr.right;
+                tmp.right = curr.right;
                 curr.left = null;
                 curr.right = next;
             }
