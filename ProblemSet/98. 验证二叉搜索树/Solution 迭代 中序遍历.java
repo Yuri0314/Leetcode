02b1fb05560a078f6dd9403b378a30f5d37551c5
陆洵 <huitianyu.hty@alibaba-inc.com>
2021-08-11 15:55:10 +0800

    98. 验证二叉搜索树

---
+++ ProblemSet/98. 验证二叉搜索树/Solution 迭代 中序遍历.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        long pre = Long.MIN_VALUE;
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= pre) return false;
+            pre = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
