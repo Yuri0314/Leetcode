b0030941da1baf643d5473cd3a3b8f1820aa877c
xtl4350 <ix22824953@sina.com>
2021-10-18 10:44:31 +0800

    fix 堆排序 python solution

--- ProblemSet/912.sort-an-array/python/912.sort-an-array 堆排序.py
+++ ProblemSet/912.sort-an-array/python/912.sort-an-array 堆排序.py
@@ -7,30 +7,29 @@
 # @lc code=start
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        self.nums = nums
         self.n = len(nums)
-        self.heapSort()
-        return self.nums
+        self.heapSort(nums)
+        return nums
 
-    def heapSort(self) -> None:
+    def heapSort(self, nums: List[int]) -> None:
         for i in range((self.n >> 1) - 1, -1, -1):
-            self.maxHeapify(i, self.n)
+            self.maxHeapify(nums, i, self.n)
         for i in range(self.n - 1, 0, -1):
-            self.nums[0], self.nums[i] = self.nums[i], self.nums[0]
-            self.maxHeapify(0, i)
+            nums[0], nums[i] = nums[i], nums[0]
+            self.maxHeapify(nums, 0, i)
 
-    def maxHeapify(self, root: int, len: int) -> None:
-        tmp = self.nums[root]
+    def maxHeapify(self, nums: List[int], root: int, len: int) -> None:
+        tmp = nums[root]
         while (root << 1) + 1 < len:
             child = (root << 1) + 1
-            if child + 1 != len and self.nums[child] < self.nums[child + 1]:
+            if child + 1 != len and nums[child] < nums[child + 1]:
                 child += 1
-            if tmp >= self.nums[child]:
+            if tmp >= nums[child]:
                 break
             else:
-                self.nums[root] = self.nums[child]
+                nums[root] = nums[child]
             root = child
-        self.nums[root] = tmp
+        nums[root] = tmp
 
 # @lc code=end
 
